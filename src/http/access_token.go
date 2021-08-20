8c80f2b67f1db7d7d3400b62393de453fbf6a63d
HunnTeRUS <otavio201378@gmail.com>
2021-08-20 20:27:57 -0300

    Creating insert access token method

--- src/http/access_token.go
+++ src/http/access_token.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 
 	"github.com/HunnTeRUS/bookstore_oauth-api/src/domain/access_token"
+	"github.com/HunnTeRUS/bookstore_oauth-api/src/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 type AccessTokenHandler interface {
 	GetById(c *gin.Context)
+	Create(c *gin.Context)
+	UpdateExpirationTime(c *gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -22,6 +25,33 @@ func NewHandler(service access_token.Service) AccessTokenHandler {
 }
 
 func (h *accessTokenHandler) GetById(c *gin.Context) {
+	returned, err := h.service.GetById(c.Param("access_token_id"))
+
+	if err != nil {
+		c.JSON(err.Code, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, returned)
+}
+
+func (h *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+
+	if err := c.ShouldBindJSON(&at); err != nil {
+		c.JSON(http.StatusBadRequest, errors.NewBadRequestError("invalid json body for create an access token"))
+		return
+	}
+
+	if err := h.service.Create(at); err != nil {
+		c.JSON(err.Code, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, at)
+}
+
+func (h *accessTokenHandler) UpdateExpirationTime(c *gin.Context) {
 	returned, err := h.service.GetById(c.Param("access_token_id"))
 
 	if err != nil {
